5db6eeb46b32aa728d3ae518b1e493733d8cacc8
microlinux <todd@toddmueller.com>
2018-03-21 19:29:23 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,122 @@
+DISCLAIMER
+
+I do not own a Tello. This code and documentation is based on the Tello SDK
+documentation as of 3/19/2018.
+
+WHAT THE BLEEP IS THIS
+
 A Python interface for the Ryze Tello drone.
+
+The tello module provides a Tello class, which interacts with the Tello API.
+
+The Tello has an IP of 192.168.10.1. The device sending commands must be
+connected to the Tello WiFi network and have an IP in the 192.168.10.0/24
+range.
+
+CREATE A MAGIC TELLO OBJECT
+
+Tello objects require a minimum of 2 parameters to initialize, the local IP
+address and port to bind.
+
+Ex. drone = tello.Tello('192.168.10.2', 8888)
+
+Methods that require distance or speed parameters expect feet or MPH. Include
+parameter imperial=False for meters and KPH.
+
+Ex. drone = tello.Tello('192.168.10.2', 8888, imperial=False)
+
+If you send a command to the Tello and it doesn't respond within .3 seconds, a
+RuntimeError is raised. You may specify the number of seconds to wait with the
+timeout parameter.
+
+Ex. drone = tello.Tello('192.168.10.2', 8888, imperial=False, timeout=.5)
+
+When you initialize a Tello object, it attempts to connect to the Tello and
+enter command mode. If this fails, a RuntimeError is raised.
+
+DO VARIOUS THINGS
+
+Once initialized, a number of methods are available to send commands to the
+Tello. It will respond with 'OK', 'FALSE' or a numeric value, which the methods
+return.
+
+These methods do what you'd expect. Responses are 'OK' or 'FALSE'.
+
+Tello.takeoff()
+Tello.land()
+
+Methods that perform vertical or horizontal movements require a single
+parameter, distance. Responses are 'OK' or 'FALSE'.
+
+The unit of distance is feet or meters. The SDK accepts distances of 1 to 500
+centimeters. Realistically, this translates to .1 - 5 meters or .7 - 16.4 feet.
+
+Tello.move_forward(distance)
+Tello.move_backward(distance)
+Tello.move_right(distance)
+Tello.move_left(distance)
+Tello.move_up(distance)
+Tello.move_down(distance)
+
+Methods that rotate require a single parameter, degrees. The SDK accepts values
+from 1 to 360. Responses are 'OK' or 'FALSE'.
+
+Tello.rotate_cw(degrees)
+Tello.rotate_ccw(degrees)
+
+The method to set speed requires a single parameter, speed. Responses are 'OK'
+or 'FALSE'.
+
+The unit of speed is KPH or MPH. The SDK accepts speeds from 1 to 100
+centimeters/second. Realistically, this translates to .1 to 3.6 KPH or .1 to 
+2.2 MPH.
+
+Tello.set_speed(speed)
+
+The method to flip requires a single parameter, direction. The SDK accepts 'l',
+'r', 'f', 'b', 'lf', 'lb', 'rf' or 'rb'. Responses are 'OK' or 'FALSE'.
+
+Tello.flip(direction)
+
+Methods that retrieve information from the Tello take no parameters. Responses
+are numeric values.
+
+Get current speed in KPH or MPH:
+Tello.get_speed()
+
+Get percent battery life remaining:
+Tello.get_battery()
+
+Get elapsed flight time in seconds:
+Tello.get_flight_time()
+
+DO THE HOKEY POKEY
+
+Put it all together, and you might do something like this.
+
+import tello
+import time
+
+drone = tello.Tello('192.168.10.1', 8888)
+
+drone.takeoff()
+time.sleep(5)
+
+drone.set_speed(2)
+time.sleep(1)
+
+drone.move_up(3)
+time.sleep(5)
+
+drone.move_forward(10)
+time.sleep(10)
+
+drone.rotate_cw(180)
+time.sleep(5)
+
+drone.move_forward(10)
+time.sleep(10)
+
+drone.land()
+
+print 'Flight time: %s' % drone.get_flight_time()
---
+++ tello.py
@@ -0,0 +1,374 @@
+"""License.
+
+Copyright 2018 Todd Mueller <firstnamelastname@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import socket
+import threading
+import time
+import traceback
+
+class Tello:
+    """Wrapper to simply interactions with the Ryze Tello drone."""
+
+    def __init__(self, local_ip, local_port, imperial=True, command_timeout=.3, tello_ip='127.0.0.1', tello_port=8889):
+        """Binds to the local IP/port and puts the Tello into command mode.
+
+        Args:
+            local_ip (str): Local IP address to bind.
+            local_port (int): Local port to bind.
+            imperial (bool): If True, speed is MPH and distance is feet.
+                             If False, speed is KPH and distance is meters.
+            command_timeout (int|float): Number of seconds to wait for a response to a command.
+            tello_ip (str): Tello IP.
+            tello_port (int): Tello port.
+
+        Raises:
+            RuntimeError: If the Tello rejects the attempt to enter command mode.
+
+        """
+
+        self.abort_flag = False
+        self.command_timeout = command_timeout
+        self.imperial = imperial
+        self.response = None
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.tello_address = (tello_ip, tello_port)
+
+        self.socket.bind((local_ip, local_port))
+
+        self.receive_thread = threading.Thread(target=self._receive_thread)
+        self.receive_thread.daemon=True
+
+        self.receive_thread.start()
+
+        if self.send_command('command') != 'OK':
+            raise RuntimeError('Tello rejected attempt to enter command mode')
+
+    def __del__(self):
+        """Closes the local socket."""
+
+        self.socket.close()
+
+    def _receive_thread(self):
+        """Listens for responses from the Tello.
+
+        Runs as a thread, sets self.response to whatever the Tello last returned.
+
+        """
+        while True:
+            try:
+                self.response, ip = self.socket.recvfrom(256)
+            except Exception:
+                break
+
+    def flip(self, direction):
+        """Flips.
+
+        Args:
+            direction (str): Direction to flip, 'l', 'r', 'f', 'b', 'lb', 'lf', 'rb' or 'rf'.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+
+        return self.send_command('flip %s' % direction)
+
+    def get_battery(self):
+        """Returns percent battery life remaining.
+
+        Returns:
+            int: Percent battery life remaining.
+
+        """
+
+        battery = self.send_command('battery?')
+
+        try:
+            battery = int(battery)
+        except:
+            pass
+
+        return battery
+
+
+    def get_flight_time(self):
+        """Returns the number of seconds elapsed during flight.
+
+        Returns:
+            int: Seconds elapsed during flight.
+
+        """
+
+        flight_time = self.send_command('time?')
+
+        try:
+            flight_time = int(flight_time)
+        except:
+            pass
+
+        return flight_time
+
+    def get_speed(self):
+        """Returns the current speed.
+
+        Returns:
+            int: Current speed in KPH or MPH.
+
+        """
+
+        speed = self.send_command('speed?')
+
+        try:
+            speed = float(speed)
+
+            if self.imperial is True:
+                speed = round((speed / 44.704), 1)
+            else:
+                speed = round((speed / 27.7778), 1)
+        except:
+            pass
+
+        return speed
+
+    def land(self):
+        """Initiates landing.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+
+        return self.send_command('land')
+
+    def move(self, direction, distance):
+        """Moves in a direction for a distance.
+
+        This method expects meters or feet. The Tello API expects distances
+        from 20 to 500 centimeters.
+
+        Metric: .02 to 5 meters
+        Imperial: .7 to 16.4 feet
+
+        Args:
+            direction (str): Direction to move, 'forward', 'back', 'right' or 'left'.
+            distance (int|float): Distance to move.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+
+        distance = float(distance)
+
+        if self.imperial is True:
+            distance = int(round(distance * 30.48))
+        else:
+            distance = int(round(distance * 100))
+
+        return self.send_command('%s %s' % (direction, distance))
+
+    def move_backward(self, distance):
+        """Moves backward for a distance.
+
+        See comments for Tello.move().
+
+        Args:
+            distance (int): Distance to move.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+
+        return self.move('back', distance)
+
+    def move_down(self, distance):
+        """Moves down for a distance.
+
+        See comments for Tello.move().
+
+        Args:
+            distance (int): Distance to move.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+
+        return self.move('down', distance)
+
+    def move_forward(self, distance):
+        """Moves forward for a distance.
+
+        See comments for Tello.move().
+
+        Args:
+            distance (int): Distance to move.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+        return self.move('forward', distance)
+
+    def move_left(self, distance):
+        """Moves left for a distance.
+
+        See comments for Tello.move().
+
+        Args:
+            distance (int): Distance to move.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+        return self.move('left', distance)
+
+    def move_right(self, distance):
+        """Moves right for a distance.
+
+        See comments for Tello.move().
+
+        Args:
+            distance (int): Distance to move.
+
+        """
+        return self.move('right', distance)
+
+    def move_up(self, distance):
+        """Moves up for a distance.
+
+        See comments for Tello.move().
+
+        Args:
+            distance (int): Distance to move.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+
+        return self.move('up', distance)
+
+    def send_command(self, command):
+        """Sends a command to the Tello and waits for a response.
+
+        If self.command_timeout is exceeded before a response is received,
+        a RuntimeError exception is raised.
+
+        Args:
+            command (str): Command to send.
+
+        Returns:
+            str: Response from Tello.
+
+        Raises:
+            RuntimeError: If no response is received within self.timeout seconds.
+
+        """
+
+        self.abort_flag = False
+        timer = threading.Timer(self.command_timeout, self.set_abort_flag)
+
+        self.socket.sendto(command.encode('utf-8'), self.tello_address)
+
+        timer.start()
+
+        while self.response is None:
+            if self.abort_flag is True:
+                raise RuntimeError('No response to command')
+
+        timer.cancel()
+
+        response = self.response.decode('utf-8')
+        self.response = None
+
+        return response
+
+    def set_abort_flag(self):
+        """Sets self.abort_flag to True.
+
+        Used by the timer in Tello.send_command() to indicate to that a response
+        timeout has occurred.
+
+        """
+
+        self.abort_flag = True
+
+    def set_speed(self, speed):
+        """Sets speed.
+
+        This method expects KPH or MPH. The Tello API expects speeds from
+        1 to 100 centimeters/second.
+
+        Metric: .1 to 3.6 KPH
+        Imperial: .1 to 2.2 MPH
+
+        Args:
+            speed (int|float): Speed.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+
+        speed = float(speed)
+
+        if self.imperial is True:
+            speed = int(round(speed * 44.704))
+        else:
+            speed = int(round(speed * 27.7778))
+
+        return self.send_command('speed %s' % speed)
+
+    def takeoff(self):
+        """Initiates take-off.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+
+        return self.send_command('takeoff')
+
+    def rotate_cw(self, degrees):
+        """Rotates clockwise.
+
+        Args:
+            degrees (int): Degrees to rotate, 1 to 360.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+
+        return self.send_command('cw %s' % degrees)
+
+    def rotate_ccw(self, degrees):
+        """Rotates counter-clockwise.
+
+        Args:
+            degrees (int): Degrees to rotate, 1 to 360.
+
+        Returns:
+            str: Response from Tello, 'OK' or 'FALSE'.
+
+        """
+        return self.send_command('ccw %s' % degrees)
