d8cf8b26520418afba977d28c2d54e4ccec50911
Boi <you@example.com>
2018-07-22 22:51:27 +1200

    added functionality and played with response times and recognitions

--- speech.py
+++ speech.py
@@ -18,13 +18,10 @@ while True:
         print ( "Speak to the computer what commands you want your tello drone to do" )
         audio = r.listen(source)
 
-    if "get speed" in r.recognize_google(audio):
+    if "speed" in r.recognize_google(audio):
         # Make a drone command to obtain speed
-        print ( "Sending landing request" )
         print ( drone.get_speed() )
-        print ( "Landing request sent" )
 
-    elif "battery" in r.recognize_google(audio):
+    if "battery" in r.recognize_google(audio):
         # Make a drone command to obtain battery
         print ( drone.get_battery() )
-        print ( "Takeoff request sent" )
