aac5a6dee0a0fd12876d1e7e469e3d3bfd62509f
James Peters <JimiPedros@users.noreply.github.com>
2019-02-09 16:48:27 +1300

    Update example_one.py
    simple take off and land

--- examples/example_one.py
+++ examples/example_one.py
@@ -1,24 +1,12 @@
 import tello
 import time
 
+print 'Example One: take off, pause 10 sec, land'
+
 drone = tello.Tello('192.168.10.2', 8888)
 
 drone.takeoff()
-time.sleep(5)
 
-drone.set_speed(2)
-time.sleep(1)
-
-drone.move_up(3)
-time.sleep(5)
-
-drone.move_forward(10)
-time.sleep(10)
-
-drone.rotate_cw(180)
-time.sleep(5)
-
-drone.move_forward(10)
 time.sleep(10)
 
 drone.land()
